25e87749d626a3eaedefdf73d6628110ce16be5c
Daniel Evans <me@daredoes.work>
2023-10-15 21:31:32 -0700

    get base url from env

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({mode}) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
   plugins: [react()],
   resolve: {
     alias: {
       src: "/src",
     },
   },
-});
+  base: env.BASE_URL
+}});
