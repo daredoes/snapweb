59736bbc4f8876448028e28126bc4f97b76d43d6
Daniel Evans <me@daredoes.work>
2023-11-04 00:25:04 -0700

    add env var for base url

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -5,6 +5,7 @@ const config: GatsbyConfig = {
     title: `snapweb`,
     siteUrl: `https://www.yourdomain.tld`
   },
+  pathPrefix: process.env.GATSBY_BASE_URL || undefined,
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
